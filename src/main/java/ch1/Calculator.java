85b0baf4fe7f6b69ea9db4c44230845e3952343e
yangwansu <ywsaau77@gmail.com>
2021-06-15 00:29:19 +0900

    complete chater1

---
+++ src/main/java/ch1/Calculator.java
@@ -0,0 +1,18 @@
+package ch1;
+
+public class Calculator {
+
+    public int add(int num1, int num2) {
+        int sum;
+        sum = num1 + num2;
+        return sum;
+    }
+
+    public int addAll(int ... num) {
+        int sum = 0;
+
+        return sum;
+    }
+
+
+}
